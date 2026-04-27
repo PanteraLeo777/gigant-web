b5b91db40b7b72c9471855b0aaa82abecfac104f
PanteraLeo777 <eduard.toloch@gmail.com>
2026-04-27 10:52:00 +0200

    martin masak zprva

--- nastaveni.js
+++ nastaveni.js
@@ -7,13 +7,24 @@ const nastaveni = {
         tlacitkoHistorie: "Zobrazit historii",
         tlacitkoZavrit: "Skrýt historii",
         aktualni: {
-            nadpis: "PŘIHLÁŠKY SPUŠTĚNY!",
-            text: "Registrace v systému ORIS je nyní otevřena. Těšíme se na vás ve Vizovicích!",
-            odkaz: "https://oris.ceskyorientak.cz/", // nech prázdný "", pokud nechceš odkaz
-            textOdkazu: "Odkaz na přihlášky",
+            nadpis: "Slovo stavitele Masssáku:",
+            text: `Nebojte se Valašska!
+ 
+Valašsko má spousta z vás spojeno s náročnými kopci = těžkými výjezdy + prudkými sjezdy.
+Ano, takové Valašsko skutečně je. Vždy to byl kraj drsný, kde vládli vlci a medvědi.
+ 
+Ale je i krásné svými oblými vrchy s nádhernými výhledy, protože je plné pastvin a luk. A právě to si můžete letos v plné míře užít při mistrovském závodě s hromadným startem. Výhledy budou luxusní!!!
+ 
+Jako stavitel jsem se snažil Vám, závodníkům, ukázat spíše ty krásné stránky valašských kotárů. Proto největší kopec, který za celý den absolvujete, bude již cestou na start. Tak se dostanete na široký plochý hřeben, kde je mnoho rychlých cest a zde se odehraje většina závodu. Ti nejmenší to budou mít téměř downhill zpět do centra závodu, kde bude i cíl.
+Závodníci mistrovských kategorií by mi však jistě neodpustili, kdyby na trati neměli nic „na zahřátí“. Pro ty jsou připravena krátká, ale výživná stoupání v blízkosti obce Chrastěšov.
+Závod je koncipován jako hodně kontaktní. Ve spleti rychlých cest bude náročné se v balíku správně zorientovat a mít přehozenou výhybku včas před křižovatkou. Na žádné kontrole nebude nutno se otáčet, pořád „plnou parou vpřed“! Běda tomu, kdo špatně odbočí, bude si pak připadat jako Alois Jirásek = Proti všem 😊
+Pro zpestření Vás čekají kromě rychlých pasáží náročných spíše na precizní orientaci v terénu při plné rychlosti také dva „královské“ postupy s množstvím naprosto odlišných voleb postupů, které mohou celý závod rozhodnout.`,
+            odkaz: "", // nech prázdný "", pokud nechceš odkaz
+            textOdkazu: "",
             obrazek: "letak_verze_2.jpg" // nech prázdný "", pokud nechceš obrázek
         },
         historie: [
+            { datum: "9. 4. 2026", zprava: "Registrace v systému ORIS je otevřena. https://oris.ceskyorientak.cz"},
             { datum: "4. 3. 2026", zprava: "Dal jsem si k snídani dort. Byl moc dobrej." },
             { datum: "1. 3. 2026", zprava: "Spuštěn web závodu Vizovice 2026." }
         ],
