f945b243914a6a858fd3a959462e780a0e6e1695
PanteraLeo777 <eduard.toloch@gmail.com>
2026-05-12 22:32:51 +0200

    Update nastaveni.js

--- nastaveni.js
+++ nastaveni.js
@@ -2,28 +2,28 @@ const nastaveni = {
     skrytNovinky: false, // true = schová novinky na obou webech, false = budou videt
 
     // ČESKÁ VERZE
-    cs: {https://www.rajce.idnes.cz/ludekvalik/album/260509-mtbo-vizovice?utm_source=notification&utm_medium=email&utm_campaign=album-created
+    cs: {
         nadpisSekce: "Aktuality",
         tlacitkoHistorie: "Zobrazit historii",
         tlacitkoZavrit: "Skrýt historii",
         aktualni: {
-            nadpis: "Slovo stavitele Masssáku:",
-            text: `Nebojte se Valašska!
- 
-Valašsko má spousta z vás spojeno s náročnými kopci = těžkými výjezdy + prudkými sjezdy.
-Ano, takové Valašsko skutečně je. Vždy to byl kraj drsný, kde vládli vlci a medvědi.
- 
-Ale je i krásné svými oblými vrchy s nádhernými výhledy, protože je plné pastvin a luk. A právě to si můžete letos v plné míře užít při mistrovském závodě s hromadným startem. Výhledy budou luxusní!!!
- 
-Jako stavitel jsem se snažil Vám, závodníkům, ukázat spíše ty krásné stránky valašských kotárů. Proto největší kopec, který za celý den absolvujete, bude již cestou na start. Tak se dostanete na široký plochý hřeben, kde je mnoho rychlých cest a zde se odehraje většina závodu. Ti nejmenší to budou mít téměř downhill zpět do centra závodu, kde bude i cíl.
-Závodníci mistrovských kategorií by mi však jistě neodpustili, kdyby na trati neměli nic „na zahřátí“. Pro ty jsou připravena krátká, ale výživná stoupání v blízkosti obce Chrastěšov.
-Závod je koncipován jako hodně kontaktní. Ve spleti rychlých cest bude náročné se v balíku správně zorientovat a mít přehozenou výhybku včas před křižovatkou. Na žádné kontrole nebude nutno se otáčet, pořád „plnou parou vpřed“! Běda tomu, kdo špatně odbočí, bude si pak připadat jako Alois Jirásek = Proti všem 😊
-Pro zpestření Vás čekají kromě rychlých pasáží náročných spíše na precizní orientaci v terénu při plné rychlosti také dva „královské“ postupy s množstvím naprosto odlišných voleb postupů, které mohou celý závod rozhodnout.`,
-            odkaz: "", // nech prázdný "", pokud nechceš odkaz
-            textOdkazu: "",
+            nadpis: "Už jsou fotky!",
+            text: ` První fotky jsou tady a další ještě přijdou. Fotky z vyhlášení jsou od našho fotografa Karla Smolena dole ve fotogalerii.`,
+            odkaz: "https://www.rajce.idnes.cz/ludekvalik/album/260509-mtbo-vizovice?utm_source=notification&utm_medium=email&utm_campaign=album-created", // nech prázdný "", pokud nechceš odkaz
+            textOdkazu: "Fotky Luděk Valík",
             obrazek: "letak_verze_2.jpg" // nech prázdný "", pokud nechceš obrázek
         },
         historie: [
+            { datum: "28. 4. 2026", zprava: `Slovo stavitele massáku: Nebojte se Valašska!
+        Valašsko má spousta z vás spojeno s náročnými kopci = těžkými výjezdy + prudkými sjezdy.
+        Ano, takové Valašsko skutečně je. Vždy to byl kraj drsný, kde vládli vlci a medvědi.
+         
+        Ale je i krásné svými oblými vrchy s nádhernými výhledy, protože je plné pastvin a luk. A právě to si můžete letos v plné míře užít při mistrovském závodě s hromadným startem. Výhledy budou luxusní!!!
+         
+        Jako stavitel jsem se snažil Vám, závodníkům, ukázat spíše ty krásné stránky valašských kotárů. Proto největší kopec, který za celý den absolvujete, bude již cestou na start. Tak se dostanete na široký plochý hřeben, kde je mnoho rychlých cest a zde se odehraje většina závodu. Ti nejmenší to budou mít téměř downhill zpět do centra závodu, kde bude i cíl.
+        Závodníci mistrovských kategorií by mi však jistě neodpustili, kdyby na trati neměli nic „na zahřátí“. Pro ty jsou připravena krátká, ale výživná stoupání v blízkosti obce Chrastěšov.
+        Závod je koncipován jako hodně kontaktní. Ve spleti rychlých cest bude náročné se v balíku správně zorientovat a mít přehozenou výhybku včas před křižovatkou. Na žádné kontrole nebude nutno se otáčet, pořád „plnou parou vpřed“! Běda tomu, kdo špatně odbočí, bude si pak připadat jako Alois Jirásek = Proti všem 😊
+        Pro zpestření Vás čekají kromě rychlých pasáží náročných spíše na precizní orientaci v terénu při plné rychlosti také dva „královské“ postupy s množstvím naprosto odlišných voleb postupů, které mohou celý závod rozhodnout.`},
             { datum: "9. 4. 2026", zprava: "Registrace v systému ORIS je otevřena. https://oris.ceskyorientak.cz"},
             { datum: "4. 3. 2026", zprava: "Dal jsem si k snídani dort. Byl moc dobrej." },
             { datum: "1. 3. 2026", zprava: "Spuštěn web závodu Vizovice 2026." }
@@ -38,10 +38,10 @@ Pro zpestření Vás čekají kromě rychlých pasáží náročných spíše na
         tlacitkoHistorie: "Show History",
         tlacitkoZavrit: "Hide History",
         aktualni: {
-            nadpis: "REGISTRATION OPEN!",
-            text: "Registration via the ORIS system is now open. We look forward to seeing you in Vizovice!",
-            odkaz: "https://oris.ceskyorientak.cz/",
-            textOdkazu: "Registration link",
+            nadpis: "PHOTOS",
+            text: "The first batch of photos are at this link, with more down in the fotogallery section",
+            odkaz: "https://www.rajce.idnes.cz/ludekvalik/album/260509-mtbo-vizovice?utm_source=notification&utm_medium=email&utm_campaign=album-created",
+            textOdkazu: "Foto link",
             obrazek: "letak_verze_2.jpg"
         },
         historie: [
